fb8828b92f054aca8b5ee412b643861603cda727
Pande Putu Widya Oktapratama <widya.oktapratama@gmail.com>
2021-06-10 17:52:12 +0800

    Add pub sub receiver

---
+++ nodejs/pubsub_receive.js
@@ -0,0 +1,25 @@
+let amqp = require("amqplib/callback_api")
+
+amqp.connect("amqp://localhost",function(error0, connection) {
+    if(error0) throw error0
+
+    connection.createChannel(function(error1, channel){
+        if(error1) throw error1
+        channel.assertExchange("GLOBAL_X",'topic',{
+            durable: false,
+            autoDelete: false,
+        })
+
+        let queue = 'app'
+
+        channel.assertQueue(queue, {exclusive: false, durable: false}, function(error2){
+            if (error2) throw error2
+
+           channel.bindQueue(queue,'GLOBAL_X','user.created', )
+
+            channel.consume(queue, function(msg){
+                console.log(msg.content.toString())
+            })
+        })        
+    })
+})
